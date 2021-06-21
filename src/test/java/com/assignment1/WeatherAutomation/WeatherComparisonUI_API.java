dc4729f8fc0408cb9abe6afd5a7a130bcc9853a9
Vinita <vinita.kaushal@amadeus.com>
2021-06-21 14:01:50 +0530

    Format changes

--- src/test/java/com/assignment1/WeatherAutomation/WeatherComparisonUI_API.java
+++ src/test/java/com/assignment1/WeatherAutomation/WeatherComparisonUI_API.java
@@ -1,4 +1,5 @@
 package com.assignment1.WeatherAutomation;
+
 import com.assignment1.library.BaseTest;
 import com.assignment1.pages.Weather;
 import com.assignment1.pages.WeatherFields;
@@ -16,83 +17,85 @@ import static com.jayway.restassured.RestAssured.given;
 import static com.codeborne.selenide.Selenide.open;
 import java.util.Properties;
 
-
-public class WeatherComparisonUI_API extends BaseTest{
-  UtilityMethods util=new UtilityMethods();
-  String conditionUI;
-  String humidityUI;
-  String windUI;
-  double tempUICelsius;
-  double tempUIFah;
-  double tempAPICelsius;
-  double tempAPIFah;
-  String humidAPI;
-  Properties prop;
-  
-  @Test
-  public void launchURL() throws InterruptedException {
+public class WeatherComparisonUI_API extends BaseTest {
+	UtilityMethods util = new UtilityMethods();
+	String conditionUI;
+	String humidityUI;
+	String windUI;
+	double tempUICelsius;
+	double tempUIFah;
+	double tempAPICelsius;
+	double tempAPIFah;
+	String humidAPI;
+	Properties prop;
+	Weather weatherPage;
+	@Test
+	public void launchURL() throws InterruptedException {
 		prop = new Properties();
 		prop = UtilityMethods.OpenProperties("src/test/java/resources/env.properties");
 		open(prop.get("URL").toString());
-		Weather weatherPage = page(Weather.class);
+		weatherPage = page(Weather.class);
 		weatherPage.clickNavIcon();
 	}
-	
-  @Test(dependsOnMethods = "launchURL")
-  @Parameters({"city"})
-  public void validateWeatherCity(String city) throws InterruptedException {
-  Weather weatherPage = page(Weather.class);
-	weatherPage.clickNavItem("WEATHER");
-	weatherPage.sendCity(city);
-	weatherPage.clickCityCheckbox(city);
-	Thread.sleep(5000);
-	weatherPage.validateWeatherCity(city);
-	weatherPage.clickWeatherCity(city);
-	weatherPage.validateWeatherLeaflet(city);
-  }
-  @Test(dependsOnMethods = "validateWeatherCity")
-  @Parameters({"city"})
-  public void getWeatherData(String city) {
-	  Weather weatherPage = page(Weather.class);
-	  conditionUI=weatherPage.getWeatherCondition(city);
-	  humidityUI=weatherPage.getWeatherHumidity(city);
-	  windUI=weatherPage.getWeatherWind(city);
-	  tempUICelsius=weatherPage.getWeatherTempDegree(city);
-	  tempUIFah=weatherPage.getWeatherTempFahrenheit(city);
-	  Reporter.log(city +" Weather Condition : " + conditionUI,true);
-	  Reporter.log(city +" Humidity : " + humidityUI,true);
-	  Reporter.log(city +" Wind : " + windUI,true);
-	  Reporter.log(city +" Temperature in Celsius from UI :::: " + tempUICelsius,true);
-	  Reporter.log(city +" Temperature in Fahrenheit : " + tempUIFah,true);
-  }
-  @Test(dependsOnMethods = "getWeatherData")
-  public void closeWeatherLeaflet() {
-	  Weather weatherPage = page(Weather.class);
-	  weatherPage.clickLeafletClose();
-	  Reporter.log("Weather Validation Successful..",true);
-  }
-  @Test(dependsOnMethods="closeWeatherLeaflet")
-  @Parameters({"city"})
-  public void validateWeatherAPI(String city) {
-	  String appidValue="e72c7be33562a4d824862a5e6717d4f9";
-	  String cityValue=city;
-	  RestAssured.config = RestAssured.config().sslConfig(SSLConfig.sslConfig().relaxedHTTPSValidation());
-	  String url="https://api.openweathermap.org/data/2.5/weather";
-	  Response response = given().that().queryParam("appid",appidValue).and().queryParam("q", cityValue).when().get(url).andReturn();
-	  String res=response.getBody().asString();
-	  int resCode=response.getStatusCode();
-	  Reporter.log("The response Body is " + res,true);
-	  Assert.assertEquals(resCode,200);
-	  Reporter.log("The response Code is " + resCode,true);
-	  Message message=util.getMessages(response);
-		/* int humidA=message.getMain().getHumidity(); */
-	  double tempAPIKelvin=message.getMain().getTemp();
-	   tempAPICelsius=util.convertKelvinToDegree(tempAPIKelvin);
-		/* Reporter.log("Humidity from API : "+humidA,true); */
-	  Reporter.log("Temperature in Celsius from API ::::"+ tempAPICelsius,true);
-  }
-	
-	@Test(dependsOnMethods="validateWeatherAPI") 
+
+	@Test(dependsOnMethods = "launchURL")
+	@Parameters({ "city" })
+	public void validateWeatherCity(String city) throws InterruptedException {
+		weatherPage = page(Weather.class);
+		weatherPage.clickNavItem("WEATHER");
+		weatherPage.sendCity(city);
+		weatherPage.clickCityCheckbox(city);
+		Thread.sleep(5000);
+		weatherPage.validateWeatherCityOnMap(city);
+		weatherPage.clickWeatherCity(city);
+		weatherPage.validateWeatherLeaflet(city);
+	}
+
+	@Test(dependsOnMethods = "validateWeatherCity")
+	@Parameters({ "city" })
+	public void getWeatherData(String city) {
+		weatherPage = page(Weather.class);
+		conditionUI = weatherPage.getWeatherCondition(city);
+		humidityUI = weatherPage.getWeatherHumidity(city);
+		windUI = weatherPage.getWeatherWind(city);
+		tempUICelsius = weatherPage.getWeatherTempDegree(city);
+		tempUIFah = weatherPage.getWeatherTempFahrenheit(city);
+		Reporter.log(city + " Weather Condition : " + conditionUI, true);
+		Reporter.log(city + " Humidity : " + humidityUI, true);
+		Reporter.log(city + " Wind : " + windUI, true);
+		Reporter.log(city + " Temperature in Fahrenheit : " + tempUIFah, true);
+		Reporter.log(city + " Temperature in Celsius from UI :::: " + tempUICelsius, true);
+	}
+
+	@Test(dependsOnMethods = "getWeatherData")
+	public void closeWeatherLeaflet() {
+		weatherPage = page(Weather.class);
+		weatherPage.clickLeafletClose();
+		Reporter.log("Weather Validation on UI Successful..", true);
+	}
+
+	@Test(dependsOnMethods = "closeWeatherLeaflet")
+	@Parameters({ "city" })
+	public void validateWeatherAPI(String city) {
+		String appidValue = "e72c7be33562a4d824862a5e6717d4f9";
+		String cityValue = city;
+		RestAssured.config = RestAssured.config().sslConfig(SSLConfig.sslConfig().relaxedHTTPSValidation());
+		String url = "https://api.openweathermap.org/data/2.5/weather";
+		Response response = given().that().queryParam("appid", appidValue).and().queryParam("q", cityValue).when()
+				.get(url).andReturn();
+		String res = response.getBody().asString();
+		int resCode = response.getStatusCode();
+		Reporter.log("The response Body is " + res, true);
+		Assert.assertEquals(resCode, 200);
+		Reporter.log("The response Code is " + resCode, true);
+		Message message = util.getMessages(response);
+		double tempAPIKelvin = message.getMain().getTemp();
+		tempAPICelsius = util.convertKelvinToDegree(tempAPIKelvin);
+		Reporter.log("Temperature in Celsius from API ::::" + tempAPICelsius, true);
+		Reporter.log("Weather Validation from API Successful..", true);
+	}
+
+	@Test(dependsOnMethods = "validateWeatherAPI")
 	public void compareWeatherUIAPI() {
 		WeatherFields weatherUI = new WeatherFields();
 		WeatherFields weatherAPI = new WeatherFields();
@@ -102,14 +105,15 @@ public class WeatherComparisonUI_API extends BaseTest{
 		weatherUI.setWind(windUI);
 		weatherAPI.setCelsius(tempAPICelsius);
 		String[] tempCelsiusMagnitudes = prop.getProperty("difference").split(",");
-		double actualDifference=0.0;
+		double actualDifference = 0.0;
 		for (String ele : tempCelsiusMagnitudes) {
-			double diff=Double.parseDouble(ele);
-			Reporter.log("difference from prop file : " + ele, true);			
-			actualDifference=Math.abs(weatherUI.getCelsius()-weatherAPI.getCelsius());
-			Assert.assertTrue(actualDifference<=diff,"Actual Difference: "+actualDifference+"************"+" Difference of Temperature in Degree is not within magnitude specified");	
+			double diff = Double.parseDouble(ele);
+			Reporter.log("difference from prop file : " + ele, true);
+			actualDifference = Math.abs(weatherUI.getCelsius() - weatherAPI.getCelsius());
+			Assert.assertTrue(actualDifference <= diff, "Actual Difference: " + actualDifference + "************"
+					+ " Difference of Temperature in Degree is not within magnitude specified");
 		}
-		Reporter.log("Actual Difference: "+actualDifference,true);
-		  }
-	 
+		Reporter.log("Actual Difference: " + actualDifference, true);
+		Reporter.log("Comparison Validation completed successfully..",true);
+	}
 }
--- src/test/java/com/assignment1/pages/Weather.java
+++ src/test/java/com/assignment1/pages/Weather.java
@@ -1,4 +1,5 @@
 package com.assignment1.pages;
+
 import static com.codeborne.selenide.Selenide.actions;
 import org.junit.Assert;
 import org.openqa.selenium.By;
@@ -16,134 +17,139 @@ import java.util.Map;
 public class Weather {
 	@FindBy(xpath = "//a[@class='topnavmore']")
 	private SelenideElement NavMenuIcon;
-	
-	@FindBy(id="map_canvas")
+
+	@FindBy(id = "map_canvas")
 	private SelenideElement WeatherPage;
-	
-	@FindBy(xpath="//input[@id='searchBox']")
+
+	@FindBy(xpath = "//input[@id='searchBox']")
 	private SelenideElement WeatherPageSearchBox;
-	
-	@FindBy(xpath="//div[@class='leaflet-popup-content']")
+
+	@FindBy(xpath = "//div[@class='leaflet-popup-content']")
 	private SelenideElement CityWeatherLeaflet;
-	
-	@FindBy(xpath="//a[@class='leaflet-popup-close-button']")
+
+	@FindBy(xpath = "//a[@class='leaflet-popup-close-button']")
 	private SelenideElement CityWeatherLeafletCloseIcon;
-	
-	@FindBy(xpath="//div[@class='leaflet-popup-content']//b[contains(text(),'Wind')]")
+
+	@FindBy(xpath = "//div[@class='leaflet-popup-content']//b[contains(text(),'Wind')]")
 	private SelenideElement WeatherWindInfo;
-	
-	@FindBy(xpath="//div[@class='leaflet-popup-content']//b[contains(text(),'Condition')]")
+
+	@FindBy(xpath = "//div[@class='leaflet-popup-content']//b[contains(text(),'Condition')]")
 	private SelenideElement WeatherConditionInfo;
-	
-	@FindBy(xpath="//div[@class='leaflet-popup-content']//b[contains(text(),'Humidity')]")
+
+	@FindBy(xpath = "//div[@class='leaflet-popup-content']//b[contains(text(),'Humidity')]")
 	private SelenideElement WeatherHumidityInfo;
-	
-	@FindBy(xpath="//div[@class='leaflet-popup-content']//b[contains(text(),'Degrees')]")
+
+	@FindBy(xpath = "//div[@class='leaflet-popup-content']//b[contains(text(),'Degrees')]")
 	private SelenideElement WeatherTempInDegrees;
-	
-	@FindBy(xpath="//div[@class='leaflet-popup-content']//b[contains(text(),'Fahrenheit')]")
+
+	@FindBy(xpath = "//div[@class='leaflet-popup-content']//b[contains(text(),'Fahrenheit')]")
 	private SelenideElement WeatherTempInFahrenheit;
-	
+
 	String[] fahrenheit;
 	String[] degree;
 	String[] humid;
 	String[] condition;
 	String[] wind;
-	
+
 	public void clickNavIcon() {
 		NavMenuIcon.shouldBe(Condition.visible).click();
 	}
-	
+
 	public void clickNavItem(String item) {
-		$(By.xpath("//a[text()='"+item+"']")).shouldBe(Condition.visible).click();
-		Assert.assertTrue("Weather Page not displayed",WeatherPage.shouldBe(Condition.visible).isDisplayed());
+		$(By.xpath("//a[text()='" + item + "']")).shouldBe(Condition.visible).click();
+		Assert.assertTrue("Weather Page not displayed", WeatherPage.shouldBe(Condition.visible).isDisplayed());
 	}
-	
+
 	public void sendCity(String city) {
 		WeatherPageSearchBox.shouldBe(Condition.visible).sendKeys(city);
 	}
-	
+
 	public void clickCityCheckbox(String city) {
-		SelenideElement ele=$(By.xpath("//input[@id='"+city+"']"));
-		if(ele.isSelected()!=true) {
-		ele.shouldBe(Condition.visible).click();
-		}
-		else 
-			Reporter.log(city+" already selected..",true);
+		SelenideElement ele = $(By.xpath("//input[@id='" + city + "']"));
+		if (ele.isSelected() != true) {
+			ele.shouldBe(Condition.visible).click();
+		} else
+			Reporter.log(city + " already selected..", true);
 	}
-	
-	public void scrollDown() {
-		actions().click().sendKeys(Keys.PAGE_DOWN).build().perform();
+
+	public void validateWeatherCityOnMap(String city) {
+		SelenideElement weatherCityName = $(
+				By.xpath("//div[@class='outerContainer']//div[@class='cityText' and text()='" + city + "']"));
+		Assert.assertEquals(city, weatherCityName.getText());
+		Reporter.log("Expected City: " + city + "Actual City: " + weatherCityName.getText(), true);
 	}
-	
-	public void validateWeatherCity(String city) {
-		SelenideElement weatherCityName=$(By.xpath("//div[@class='outerContainer']//div[@class='cityText' and text()='"+city+"']"));
-		//actions().click(weatherCityName).sendKeys(Keys.PAGE_DOWN).build().perform();
-		Assert.assertEquals(city,weatherCityName.getText());
-		Reporter.log("Expected : "+city+ "Actual : "+weatherCityName.getText(),true);
-	}
-	
+
 	public void clickWeatherCity(String city) {
-		SelenideElement weatherCityName=$(By.xpath("//div[@class='outerContainer']//div[@class='cityText' and text()='"+city+"']"));
+		SelenideElement weatherCityName = $(
+				By.xpath("//div[@class='outerContainer']//div[@class='cityText' and text()='" + city + "']"));
 		weatherCityName.click();
 	}
-	
+
 	public void validateWeatherLeaflet(String city) {
-	SelenideElement leafletPlace=$(By.xpath("//div[@class='leaflet-popup-content']//span[contains(text(),'"+city+"')]"));
-	Assert.assertTrue("Expected :"+city+ "******* Actual :" + leafletPlace.getText(),leafletPlace.getText().contains(city));
+		SelenideElement leafletPlace = $(
+				By.xpath("//div[@class='leaflet-popup-content']//span[contains(text(),'" + city + "')]"));
+		Assert.assertTrue("Expected :" + city + "******* Actual :" + leafletPlace.getText(),
+				leafletPlace.getText().contains(city));
 	}
-	
-	
+
 	public String getWeatherCondition(String city) {
-		SelenideElement cityWeatherLeaflet=$(By.xpath("//span[contains(text(),'"+city+"')]/ancestor::div[@class='leaflet-popup-content']"));
-		if(cityWeatherLeaflet.isDisplayed()) {
-			Assert.assertTrue("Condition Info not displayed",WeatherConditionInfo.shouldBe(Condition.visible).isDisplayed());
+		SelenideElement cityWeatherLeaflet = $(
+				By.xpath("//span[contains(text(),'" + city + "')]/ancestor::div[@class='leaflet-popup-content']"));
+		if (cityWeatherLeaflet.isDisplayed()) {
+			Assert.assertTrue("Condition Info not displayed",
+					WeatherConditionInfo.shouldBe(Condition.visible).isDisplayed());
 		}
-		condition=WeatherConditionInfo.getText().split(":");
-		return condition[1];		
+		condition = WeatherConditionInfo.getText().split(":");
+		return condition[1];
 	}
-	
+
 	public String getWeatherWind(String city) {
-		SelenideElement cityWeatherLeaflet=$(By.xpath("//span[contains(text(),'"+city+"')]/ancestor::div[@class='leaflet-popup-content']"));
-		if(cityWeatherLeaflet.isDisplayed()) {
-			Assert.assertTrue("Wind Info not displayed",WeatherWindInfo.shouldBe(Condition.visible).isDisplayed());
+		SelenideElement cityWeatherLeaflet = $(
+				By.xpath("//span[contains(text(),'" + city + "')]/ancestor::div[@class='leaflet-popup-content']"));
+		if (cityWeatherLeaflet.isDisplayed()) {
+			Assert.assertTrue("Wind Info not displayed", WeatherWindInfo.shouldBe(Condition.visible).isDisplayed());
 		}
-		wind=WeatherWindInfo.getText().split(":");
-		return wind[1];		
+		wind = WeatherWindInfo.getText().split(":");
+		return wind[1];
 	}
-	
+
 	public String getWeatherHumidity(String city) {
-		SelenideElement cityWeatherLeaflet=$(By.xpath("//span[contains(text(),'"+city+"')]/ancestor::div[@class='leaflet-popup-content']"));
-		if(cityWeatherLeaflet.isDisplayed()) {
-			Assert.assertTrue("Humidity Info not displayed",WeatherHumidityInfo.shouldBe(Condition.visible).isDisplayed());
+		SelenideElement cityWeatherLeaflet = $(
+				By.xpath("//span[contains(text(),'" + city + "')]/ancestor::div[@class='leaflet-popup-content']"));
+		if (cityWeatherLeaflet.isDisplayed()) {
+			Assert.assertTrue("Humidity Info not displayed",
+					WeatherHumidityInfo.shouldBe(Condition.visible).isDisplayed());
 		}
-		humid =WeatherHumidityInfo.getText().split(":");
-		String[] humidvalue=humid[1].split("%");
-		// int i = Integer.parseInt(humidvalue[0]);
-		return humidvalue[0];		
+		humid = WeatherHumidityInfo.getText().split(":");
+		String[] humidvalue = humid[1].split("%");
+		return humidvalue[0];
 	}
-	
+
 	public double getWeatherTempDegree(String city) {
-		SelenideElement cityWeatherLeaflet=$(By.xpath("//span[contains(text(),'"+city+"')]/ancestor::div[@class='leaflet-popup-content']"));
-		if(cityWeatherLeaflet.isDisplayed()) {
-			Assert.assertTrue("Humidity Info not displayed",WeatherTempInDegrees.shouldBe(Condition.visible).isDisplayed());
+		SelenideElement cityWeatherLeaflet = $(
+				By.xpath("//span[contains(text(),'" + city + "')]/ancestor::div[@class='leaflet-popup-content']"));
+		if (cityWeatherLeaflet.isDisplayed()) {
+			Assert.assertTrue("Humidity Info not displayed",
+					WeatherTempInDegrees.shouldBe(Condition.visible).isDisplayed());
 		}
-		degree=WeatherTempInDegrees.getText().split(":");
-		String TempDegreevalue=degree[1];
-		double i = Double.parseDouble(TempDegreevalue); 
-		return 	i;
+		degree = WeatherTempInDegrees.getText().split(":");
+		String TempDegreevalue = degree[1];
+		double i = Double.parseDouble(TempDegreevalue);
+		return i;
 	}
-	
+
 	public double getWeatherTempFahrenheit(String city) {
-		SelenideElement cityWeatherLeaflet=$(By.xpath("//span[contains(text(),'"+city+"')]/ancestor::div[@class='leaflet-popup-content']"));
-		if(cityWeatherLeaflet.isDisplayed()) {
-			Assert.assertTrue("Humidity Info not displayed",WeatherTempInFahrenheit.shouldBe(Condition.visible).isDisplayed());
+		SelenideElement cityWeatherLeaflet = $(
+				By.xpath("//span[contains(text(),'" + city + "')]/ancestor::div[@class='leaflet-popup-content']"));
+		if (cityWeatherLeaflet.isDisplayed()) {
+			Assert.assertTrue("Humidity Info not displayed",
+					WeatherTempInFahrenheit.shouldBe(Condition.visible).isDisplayed());
 		}
-		fahrenheit=WeatherTempInFahrenheit.getText().split(":");
-		Reporter.log(fahrenheit[1],true);
-		String value=fahrenheit[1];
-		double i = Double.parseDouble(value); 
-		return 	i;	
+		fahrenheit = WeatherTempInFahrenheit.getText().split(":");
+		Reporter.log(fahrenheit[1], true);
+		String value = fahrenheit[1];
+		double i = Double.parseDouble(value);
+		return i;
 	}
 
 	public void clickLeafletClose() {
